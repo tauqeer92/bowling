6d71f156ac7df24c5fdda3a663609750b840786c
Tauqeer Shoaib <tauqeershoaib@gmail.com>
2023-01-30 16:30:08 +0000

    Roll class throws error if more than 2 rolls

--- bowling/rolls.js
+++ bowling/rolls.js
@@ -1,19 +1,31 @@
 class Rolls {
     constructor () {
         this.rolls = []
-
-    }
-
-    roll(score) {
-        this.rolls.push(score)
     }
 
     roll_count() {
         return this.rolls.length
     }
 
+    roll(score) {
+        if (this.roll_count() < 2) {
+            this.rolls.push(score)
+        }
+        
+        else {
+            throw new Error('Too Many Rolls')
+        }
+    }
 
 }
+
+// throw new Error('Maximum amount of rolls reached')
+
+// const rolls = new Rolls()
+// rolls.roll(1)
+// rolls.roll(7)
+// const third = rolls.roll(3)
+// console.log(third)
 
 module.exports = Rolls;
 
--- bowling_tests/rolls.test.js
+++ bowling_tests/rolls.test.js
@@ -5,5 +5,11 @@ describe('Roll Count', () => {
         const rolls = new Rolls()
         expect(rolls.roll_count()).toBe(0)
     })
+    it('should throw error when input 3 rolls', () => {
+        const rolls = new Rolls()
+        rolls.roll(1)
+        rolls.roll(7)
+        expect(() => rolls.roll(3)).toThrow(Error)
+    })
 })
 
