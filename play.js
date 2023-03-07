fc88bbd7057219348f2b0a6666d3b74954a24695
Tauqeer Shoaib <tauqeershoaib@gmail.com>
2023-03-07 21:45:07 +0000

    adds correct frame scores with multiple strikes

--- play.js
+++ play.js
@@ -1,47 +1,66 @@
 class Play {
     constructor(rolls, scores) {
         this.pins = document.querySelectorAll('.score');
-        this.frameRolls = document.querySelectorAll('.roll')
-        this.frameScore = document.querySelectorAll('.frame-score')
-        this.totalScore = document.querySelector('.total-score')
-        this.rolls = rolls
-        this.scores = scores
+        this.individualFrame = document.querySelectorAll('.frame');
+        this.frameRolls = document.querySelectorAll('.frame .rolls .roll');
+        this.frameScore = document.querySelectorAll('.frame-score');
+        this.totalScore = document.querySelector('.total-score');
+        this.rolls = rolls;
+        this.scores = scores;
+        this.index = [];
     }
-
-    // When I score 10, it's not added to list of scores yet, so this.scores.readScore() is undefined
-    // If I index the roll in the rolls class array this.frames, if there's multiple 10's in the array, it will just index the first one
-    // I need to make the score unique in this class, by having an array of objects
-    // Or I can use the div by finding out which frame it is, index the the list of scores array if it exists and it to the frame score
 
     roll() {
         console.log(this.frameScore)
         this.pins.forEach(score => {
             score.addEventListener('click', () => {
-                for (let i = 0; i < this.frameRolls.length; i++) {
-                    if (this.frameRolls[i].innerHTML == "") {
-                        this.frameRolls[i].append(score.value)
+                for (let i = 0; i < this.individualFrame.length; i++) {
+                    console.log('A')
+                    if (this.individualFrame[i].querySelector('.rolls .roll.one').innerHTML == "" && this.individualFrame[i].querySelector('.rolls .roll.two').innerHTML == "" && score.value == 10) {
+                        console.log('B')
+                        this.individualFrame[i].querySelector('.roll.two').append(score.value)
+                        this.rolls.addRoll(parseInt(score.value))
+                        this.rolls.addFrame()
+                        this.scores.calculate()
+                        this.rolls.clearRolls()
+                        console.log('C')
+                        this.index.push(i)
+                        break
+                    }
+
+                    else if (this.individualFrame[i].querySelector('.rolls .roll.one').innerHTML == "" && this.individualFrame[i].querySelector('.rolls .roll.two').innerHTML == "" && score.value != 10) {
+                        this.individualFrame[i].querySelector('.roll.one').append(score.value)
+                        this.rolls.addRoll(parseInt(score.value))
+                        break
+                    }
+
+                    else if (this.individualFrame[i].querySelector('.rolls .roll.one').innerHTML != "" && this.individualFrame[i].querySelector('.rolls .roll.two').innerHTML == "" && score.value != 10) {
+                        this.individualFrame[i].querySelector('.roll.two').append(score.value)
+                        this.rolls.addRoll(parseInt(score.value))
+                        this.rolls.addFrame()
+                        this.scores.calculate()
+                        this.rolls.clearRolls()
+                        this.individualFrame[i].querySelector('.frame-score').append(this.scores.readScore())
                         break
                     }
                 }
-                this.rolls.addRoll(parseInt(score.value))
 
-                if (this.rolls.rollCount() == 2) {
-                    console.log('A')
-                    console.log(`This is the frame score ${this.rolls.showFrames()}`)
-                    this.rolls.addFrame()
-                    this.scores.calculate()
-                    this.rolls.clearRolls()
-                    console.log(this.rolls.showFrames())
-                    console.log(this.scores.readScore())
-                    for (let i = 0; i < this.frameScore.length; i++) {
-                        if (this.frameScore[i].innerHTML == "") {
-                            this.frameScore[i].append(this.scores.readScore())
-                            break
+                if (this.index.length > 0) {
+                    this.index.forEach(index => {
+                        if (index != null && this.scores.readListOfScores()[index] != undefined) {
+                            console.log('Hellooooooooooooooooo')
+                            console.log(`This is the list of scores ${this.scores.readListOfScores()}`)
+                            console.log(`This is the score in list of scores ${this.scores.readListOfScores()[index]}`)
+                            this.individualFrame[index].querySelector('.frame-score').append(this.scores.readListOfScores()[index])
+                            this.index.shift()
+                            console.log(`This is the index ${this.index}`)
+        
                         }
-                    
-                    }
-                    this.totalScore.innerHTML = this.scores.readScore()
+    
+                    })
                 }
+                
+                
                 
             })
         })
